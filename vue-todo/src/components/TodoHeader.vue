b215866bae4b1cf3541d13e8ea0ac46e36842668
junkyuLee <dlwnsrb0216@gmail.com>
2022-02-13 13:15:52 +0900

    first push

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,20 @@
+<template>
+ <header>
+   <h1>TODO it!</h1>
+ </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h1{
+    color: rgb(65, 62, 62);
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
+</style>
+// scoped 컴포넌트 안에서만 유효
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="inputBox shadow"> 
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo"> 
+      <i class="fas fa-plus"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function(){      
+      //this는 todoinput component를 가리킴
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();      
+    },
+    clearInput : function(){
+      this.newTodoItem = '';
+    }
+    // methods를 분할
+  }
+}
+</script>
+
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+  cursor: pointer;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.fas.fa-plus{
+  color: white;
+}
+</style>
