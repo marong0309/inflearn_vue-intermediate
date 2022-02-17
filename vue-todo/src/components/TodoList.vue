54340d73b420dcef064f07c2829a75b70e140e4e
junkyuLee <dlwnsrb0216@gmail.com>
2022-02-17 22:40:07 +0900

    todolist

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
         {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
       </li>      
     </ul>
   </div>
@@ -13,6 +16,13 @@ export default {
   data: function(){
     return {
       todoItems: []
+    }
+  },
+  methods:{
+    removeTodo: function(todoItem, index){
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1) // index에서 몇개(1개)를 지우겠다
     }
   },
   created: function(){
@@ -31,5 +41,37 @@ export default {
 </script>
 
 <style>
-
+  ul {
+    list-style-type: none;
+    padding-top: 0;
+    margin-top: 0;
+    text-align: left;
+    padding-inline-start: 0;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .checkBtnCompleted{
+    color: #b3adad;
+  }
+  .textCompleted{
+    text-decoration: line-through;
+    color: #b3adad;
+  }
+  .removeBtn{
+    margin-left: auto;
+    color: #dc4343;
+  }  
 </style>
